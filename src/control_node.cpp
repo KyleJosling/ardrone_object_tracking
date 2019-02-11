4b1525aa2ac5620a18413596a035b2ab0b2c596b
Kyle Josling <kylejosling@hotmail.com>
2019-02-11 18:59:06 -0500

    Added send commmand function

--- src/control_node.cpp
+++ src/control_node.cpp
@@ -18,8 +18,7 @@ class controller {
         
         // Initialize PID controller
         // ( double dt, double max, double min, double Kp, double Kd, double Ki );
-        yawPid(0.1, 320, -320, 0.702, 4.9, 0.00006)
-        { 
+        yawPid(0.1, 320, -320, 0.702, 4.9, 0.00006) { 
 
             // Subscriber for object location
             objectSub = nH.subscribe(objectTopic, 1, &controller::objectCallback, this);
@@ -28,19 +27,17 @@ class controller {
             controlPub = nH.advertise<geometry_msgs::Twist>(controlTopic, 1000);
             takeoffPub = nH.advertise<std_msgs::Empty>(takeoffTopic, 1000);
             landingPub = nH.advertise<std_msgs::Empty>(landingTopic, 1000);
+            
+            startupRoutine();
 
-            // Sleep and then take off
-            usleep(5000000);
-            std_msgs::Empty msg; 
-            takeoffPub.publish(msg);
-            usleep(5000000);
         }
 
         ~controller() {
+            ROS_INFO("Destructor called");
             std_msgs::Empty msg; 
             landingPub.publish(msg);
         }
-
+        
         void objectCallback(const ardrone_object_tracking::ObjectMsg::ConstPtr &objectPos) {
 
             yawPVar = objectPos->x;
@@ -50,24 +47,44 @@ class controller {
             ROS_INFO("Received object position: ");
             ROS_INFO(" X: %f , Y: %f, H : %f \n", objectPos->x, objectPos->y, objectPos->height);
             ROS_INFO("Yaw output : %f", yawOutput);
-
-            // Pack and publish command
+            
+            sendCommand( 0, 0, 0, yawOutput);
+        }
+        
+        // Send command to AR Drone
+        void sendCommand( int x, int y, int z, int yaw) {
+            
+            // Linear and angular components
             geometry_msgs::Vector3 linear; 
             geometry_msgs::Vector3 angular;
             geometry_msgs::Twist command;
-             
+            
+            // Pack and publish command
+            linear.x = x;
+            linear.y = y;
+            linear.z = z;
+
             angular.x = 0;
             angular.y = 0;
-            angular.z = yawOutput;
-
-            linear.x = 0;
-            linear.y = 0;
-            linear.z = 0;
+            angular.z = yaw;
 
             command.linear = linear;
             command.angular = angular;
 
             controlPub.publish(command);
+
+        }
+
+        void startupRoutine() {
+            
+            ROS_INFO("Startup routine initiated");
+
+            // Sleep and then take off
+            usleep(5000000);
+            // Set to hover mode
+            sendCommand( 0, 0, 0, 0);
+            std_msgs::Empty msg; 
+            takeoffPub.publish(msg);
 
         }
 
